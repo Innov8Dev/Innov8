d51270758d1aa5d994f114a9595762d9b6ad4581
Innov8Dev <mvupha@gmail.com>
2016-07-11 12:56:59 +0200

    Add project files.

---
+++ Innov8/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Innov8.Startup))]
+namespace Innov8
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
